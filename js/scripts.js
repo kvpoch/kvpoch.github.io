fbe1a44e8fd62a88c79f7d4d56e484b19c50676d
kvpoch <kathypoch11@gmail.com>
2023-01-10 12:38:19 -0800

    test2

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 * Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
+*/ 
 //
 // Scripts
 // 
